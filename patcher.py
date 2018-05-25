a33aea47e706f1f0fecff28c66cfc9b2a1915586
BotoX <github@botox.bz>
2018-05-26 01:27:01 +0200

    added russian throttle algorith, it sucks (surprise) maybe you can make it better?

--- patcher.py
+++ patcher.py
@@ -203,6 +203,149 @@ class FirmwarePatcher():
         return [(ofs, pre, post)]
 
 
+    def russian_throttle(self):
+        ret = dict()
+        # Find address of eco mode, part 1 find base addr
+        sig = [0x91, 0x42, 0x01, 0xD2, 0x08, 0x46, 0x00, 0xE0, 0x10, 0x46, 0xA6, 0x4D]
+        ofs = FindPattern(self.data, sig)
+        ofs += 10
+        imm = struct.unpack('<H', self.data[ofs:ofs + 2])[0] & 0xFF
+        ofsa = ofs + imm * 4 + 4 # ZeroExtend '00' + align?
+        eco_addr = struct.unpack('<L', self.data[ofsa:ofsa + 4])[0]
+
+        ret['eco_base'] = {'ofs': ofs, 'imm': imm, 'ofsa': ofsa, 'addr': hex(eco_addr)}
+
+        # part 2, find offset of base addr
+        sig = [0x85, 0xF8, 0x34, 0x60] # STRB.W  R6, [R5, #imm12]
+        mask = [0xFF, 0xFF, 0x00, 0x0F] # mask imm12
+        ofs = FindPattern(self.data, sig, mask, ofs, 100)
+        imm = struct.unpack('<HH', self.data[ofs:ofs + 4])[1] & 0x0FFF
+        eco_addr += imm
+
+        ret['eco_addr'] = {'ofs': ofs, 'imm': imm, 'addr': hex(eco_addr)}
+
+
+        sig = [0xF0, 0xB5, 0x25, 0x4A, 0x00, 0x24, 0xA2, 0xF8, 0xEC, 0x40, 0x24, 0x49, 0x4B, 0x79, 0x00, 0x2B,
+               0x3E, 0xD1, 0x23, 0x4D, 0x2F, 0x68, 0x23, 0x4E, 0x23, 0x4B, 0x00, 0x2F, 0x39, 0xDB, None, 0x64,
+               0x01, 0x24, 0x74, 0x82, 0x32, 0x38, 0x01, 0xD5, 0x00, 0x20, 0x02, 0xE0, 0x7D, 0x28, 0x00, 0xDD,
+               0x7D, 0x20, 0xB2, 0xF8, 0xEC, 0x60, 0x7D, 0x24, 0x26, 0xB1, 0xB2, 0xF8, 0xEC, 0x20, 0x01, 0x2A,
+               0x0B, 0xD0, 0x13, 0xE0, 0xD1, 0xE9, None, 0x21, 0x52, 0x1A, 0x42, 0x43, 0x92, 0xFB, 0xF4, 0xF0,
+               0x08, 0x44, 0x29, 0x68, 0x02, 0xF0, None, 0xFB, 0x08, 0xE0, 0x4A, 0x8C, 0x89, 0x8C, 0x52, 0x1A,
+               0x42, 0x43, 0x92, 0xFB, 0xF4, 0xF0, 0x40, 0x18, 0x00, 0xD5, 0x00, 0x20, 0x19, 0x68, 0x09, 0x1A,
+               0x19, 0x68, 0x01, 0xD5, 0x41, 0x1A, 0x00, 0xE0, 0x09, 0x1A, 0x4F, 0xF4, 0x96, 0x72, 0x91, 0x42,
+               0x05, 0xDD, 0x19, 0x68, 0x81, 0x42, 0x00, 0xDD, 0x52, 0x42, 0x18, 0x68, 0x10, 0x44, 0x18, 0x60,
+               0xF0, 0xBD, 0x1C, 0x60, 0x74, 0x82, 0xF0, 0xBD, *[None] * 4 * 5]
+        ofs = FindPattern(self.data, sig)
+
+        ofsa = ofs + len(sig) - (4 * 5)
+        addr1, addr2, addr3, addr4, addr5 = struct.unpack('<LLLLL', self.data[ofsa:ofsa + 20])
+
+        # STRH.W (T2)  Rt, [Rn, #imm12]
+        addr1_ofs1 = struct.unpack('<H', self.data[ofs + 6 + 2:ofs + 6 + 2 + 2])[0] & 0xFFF
+
+        # LDRB (T1)  Rt, [Rn, #imm5]
+        addr2_ofs1 = (struct.unpack('<H', self.data[ofs + 12:ofs + 12 + 2])[0] >> 6) & 0x1F
+
+        # STR (T1)  Rt, [Rn, #imm5]
+        addr2_ofs2 = (struct.unpack('<H', self.data[ofs + 30:ofs + 30 + 2])[0] >> 6) & 0x1F
+        addr2_ofs2 *= 4 # ZeroExtend '00'
+
+        # STRH (T1)  Rt, [Rn, #imm5]
+        addr4_ofs1 = (struct.unpack('<H', self.data[ofs + 34:ofs + 34 + 2])[0] >> 6) & 0x1F
+        addr4_ofs1 *= 2 # ZeroExtend '0'
+
+        ret['addrs'] = {
+                        '1': [hex(addr1), hex(addr1 + addr1_ofs1)],
+                        '2': [hex(addr2), hex(addr2 + addr2_ofs1), hex(addr2 + addr2_ofs2)],
+                        '3': [hex(addr3)],
+                        '4': [hex(addr4), hex(addr4 + addr4_ofs1)],
+                        '5': [hex(addr5)]
+                        }
+
+        asm = f'''
+                LDR    R3, ={hex(addr2 + addr2_ofs1)}
+                LDRB   R3, [R3]
+                CBNZ   R3, loc_ret
+                AND    R2, R3, #0xFF
+                LDR    R3, ={hex(addr3)}
+                LDR    R1, [R3]
+                CMP    R1, #0
+                BLT    loc_1
+                PUSH   {{R4, R5}}
+                LDR    R1, ={hex(addr4 + addr4_ofs1)}
+                LDR    R5, ={hex(addr2 + addr2_ofs2)}
+                MOVS   R4, #1
+                SUBS   R0, #0x32
+                STR    R2, [R5]
+                STRH   R4, [R1]
+                BMI    loc_3
+                LDR    R2, ={hex(eco_addr)}
+                CMP    R0, #0x7D
+                LDRB   R2, [R2]
+                IT     GE
+                MOVGE  R0, #0x7D
+                CMP    R2, R4
+                BEQ    loc_2
+                MOVS   R3, #0x96
+                MUL    R3, R3, R0
+                LDR    R2, ={hex(addr5)}
+                STR    R3, [R2]
+
+                loc_popret:
+                POP    {{R4, R5}}
+
+                loc_ret:
+                BX     LR
+
+                loc_1:
+                LDR    R1, ={hex(addr5)}
+                ADD.W  R3, R3, #0x1580
+                ADDS   R3, #0x12
+                STR    R2, [R1]
+                STRH   R2, [R3]
+                BX     LR
+
+                loc_2:
+                MOVW   R4, #0x1AF4
+                MOVS   R2, #0x64
+                MUL    R2, R2, R0
+                LDR    R1, ={hex(addr5)}
+                STR    R2, [R1]
+                LDR    R2, [R3]
+                CMP    R2, R4
+                BLE    loc_popret
+                LDR    R3, [R3]
+                LDR    R2, [R1]
+                SUB.W  R3, R3, #0x1AE0
+                SUBS   R3, #0x14
+                ADD.W  R3, R3, R3, LSL#2
+                SUB.W  R3, R2, R3, LSL#1
+                STR    R3, [R1]
+                B      loc_popret
+
+                loc_3:
+                LDR    R3, ={hex(addr5)}
+                MVN    R2, #0x9
+                STR    R2, [R3]
+                B      loc_popret
+        '''
+
+        res = self.ks.asm(asm)
+        assert len(res[0]) <= len(sig), 'new code larger than old code, this won\'t work'
+        assert len(res[0]) == 164, 'hardcoded size safety check, if you haven\'t changed the ASM then something is wrong'
+
+        # pad with zero for no apparent reason
+        padded = bytes(res[0]).ljust(len(sig), b'\x00')
+
+        ret['len_sig'] = len(sig)
+        ret['len_res'] = len(res[0])
+        ret['res_inst'] = res[1]
+
+        self.data[ofs:ofs+len(padded)] = bytes(padded)
+
+        return ret
+
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
@@ -226,6 +369,7 @@ if __name__ == "__main__":
     cfw.instant_eco_switch()
     cfw.boot_with_eco()
     cfw.cruise_control_delay(5)
+    #cfw.russian_throttle()
 
     with open(sys.argv[2], 'wb') as fp:
         fp.write(cfw.data)
